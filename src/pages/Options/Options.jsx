75a4855778c5a8c78beafa408f0b3c407325bd91
George Church <15802979+gchurch@users.noreply.github.com>
2021-06-16 16:33:06 +0100

    Replaced typescript options file with javascript version.

---
+++ src/pages/Options/Options.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Options.css';
+
+export default class Options extends Component {
+    render() {
+        return (
+            <div>OPTIONS PAGE</div>
+        )
+    }
+}
