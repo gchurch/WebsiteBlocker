3933c8821e254138055cccc7427bee44d09d2d07
George Church <15802979+gchurch@users.noreply.github.com>
2021-06-20 13:50:13 +0100

    Made change to popup text.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,7 +7,7 @@ export default class Popup extends Component {
     return (
       <div>
         <h1>Website Blocker</h1>
-        <p>Click the button below to go to the options page, where you can block and unblock websites.</p>
+        <p>Click the button below to go to the options page.</p>
         <button onClick={this.openOptions}>Options</button>
       </div>
     );
