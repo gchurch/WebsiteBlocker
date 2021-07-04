7fca1fef475c72d9a87e40167dec4ac286405b71
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-04 13:19:45 +0100

    The popup now displays now many websites are currently being blocked.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import './Popup.css';
 
 export default class Popup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { numberOfBlockedWebsites: 0 };
+  }
+
+  componentDidMount() {
+    this.calculateNumberOfBlockedWebsites();
+  }
+
+  calculateNumberOfBlockedWebsites() {
+    chrome.declarativeNetRequest.getDynamicRules(
+      (rules) => {
+        this.setState({ numberOfBlockedWebsites: rules.length });
+      }
+    );
+  }
+
   render() {
     return (
       <div>
         <h1>Website Blocker</h1>
+        <p>Currently blocking {this.state.numberOfBlockedWebsites} website(s).</p>
         <button onClick={this.openOptions}>Settings</button>
       </div>
     );
