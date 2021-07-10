23fef8dd68d948033b6eef2ff429583af4f4f981
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-10 16:16:30 +0100

    Background script basic working usage of storage and alarms APIs.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,20 @@
 chrome.runtime.onInstalled.addListener(function () {
     setInterval(() => {
         console.log("Hello World!");
+        var key = "a";
+        var value = "1";
+        chrome.storage.local.set({ key: value }, function () {
+            console.log('Value is set to ' + value);
+        });
     }, 3000);
+
+    var alarmName = 'remindme';
+
+    chrome.alarms.onAlarm.addListener(function (alarm) {
+        console.log("Got an alarm!", alarm);
+    });
+
+    chrome.alarms.create(alarmName, {
+        delayInMinutes: 0.01, periodInMinutes: 0.01
+    });
 });
