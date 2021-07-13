432ae151996cd612dd53a3352039c19d24818f8b
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-13 16:41:34 +0100

    The background script now updates the blocking rules but with no changes when the extension is installed. Hopefully this will stop the reinstalling bug that I discovered.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,12 +1,9 @@
 chrome.runtime.onInstalled.addListener(function () {
-    console.log("Hello World!");
-
-    const key = "123";
-    const value = 25;
-    chrome.storage.local.set({ [key]: value }, function () {
-        console.log(key + " set to " + value);
-        chrome.storage.local.get(key, function (data) {
-            console.log("retrieved " + key + " from storage with value " + data[key]);
-        })
-    })
+    var updateRuleOptions = {
+        removeRuleIds: [],
+        addRules: []
+    }
+    chrome.declarativeNetRequest.updateDynamicRules(updateRuleOptions, () => {
+        console.log("Updated blocking rules with no changes.");
+    });
 });
