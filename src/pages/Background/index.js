9a7262a2667dd09def2936d1b665d1cf1f34d06a
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-13 16:16:35 +0100

    Got chrome storage example in background script actually properly working now. I was getting confused by javascript objects.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,9 +1,9 @@
 chrome.runtime.onInstalled.addListener(function () {
     console.log("Hello World!");
 
-    const key = "a";
-    const value = 1;
-    chrome.storage.local.set({ key: value }, function () {
+    const key = "123";
+    const value = 25;
+    chrome.storage.local.set({ [key]: value }, function () {
         console.log(key + " set to " + value);
         chrome.storage.local.get(key, function (data) {
             console.log("retrieved " + key + " from storage with value " + data[key]);
