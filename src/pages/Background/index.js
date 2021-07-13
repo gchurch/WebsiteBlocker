e26cb7612a1253fe9763ed9a1df50285308d8260
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-13 15:39:09 +0100

    got chrome storage API is working properly in background script.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,10 +1,12 @@
 chrome.runtime.onInstalled.addListener(function () {
-    setInterval(() => {
-        console.log("Hello World!");
-        var key = "a";
-        var value = "1";
-        chrome.storage.local.set({ key: value }, function () {
-            console.log('Value is set to ' + value);
-        });
-    }, 3000);
+    console.log("Hello World!");
+
+    const key = "a";
+    const value = 1;
+    chrome.storage.local.set({ key: value }, function () {
+        console.log(key + " set to " + value);
+        chrome.storage.local.get(key, function (data) {
+            console.log("retrieved " + key + " from storage with value " + data[key]);
+        })
+    })
 });
