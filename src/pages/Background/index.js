f5e8e989b44b83409f61267a6ff48990473ac9c0
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-15 16:03:48 +0100

    Stopped deleting all blocked websites when onInstalled occurs as this happens during updates.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,17 +1,3 @@
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeNetRequest.getDynamicRules((rules) => {
-        console.log("Retrieved existing rules.");
-        var existingRuleIds = [];
-        for (var rule of rules) {
-            existingRuleIds.push(rule.id)
-        }
-        console.log(existingRuleIds);
-        var updateRuleOptions = {
-            removeRuleIds: existingRuleIds,
-            addRules: []
-        }
-        chrome.declarativeNetRequest.updateDynamicRules(updateRuleOptions, () => {
-            console.log("Updated rules.");
-        });
-    });
+    console.log("Hello World!");
 });
