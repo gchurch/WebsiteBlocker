8c224815d051edc59a5df50ec466c22ff42e6c43
George Church <15802979+gchurch@users.noreply.github.com>
2021-07-17 10:55:13 +0100

    In the background script, the dynamic rules are updated but with no changes. Hopefully this will fix the bug I noticed before where when you reinstall an extension the dynamic rules remain but they don't seem to work.

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,3 +1,10 @@
 chrome.runtime.onInstalled.addListener(function () {
     console.log("Hello World!");
+    var updateRuleOptions = {
+        removeRuleIds: [],
+        addRules: []
+    }
+    chrome.declarativeNetRequest.updateDynamicRules(updateRuleOptions, () => {
+        console.log("Refreshed blocking rules.");
+    });
 });
